553ff2d1b026cc3da8a2bcc6270e14dcd3c17180
pat mcb <pmcbrien@users.noreply.github.com>
2020-04-09 15:27:53 -0400

    Update snoo.py

--- snoo.py
+++ snoo.py
@@ -17,18 +17,17 @@ def paginate(method, **kwargs):
 ec2_client = boto3.client('ec2')
 regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
 #print(regions)
-
-for region in ec2_client.describe_regions()['Regions']:
-    region_name = region['RegionName']
-    #
-    org_client = boto3.client('organizations')
-    for account in paginate(org_client.list_accounts):
-        #print (" AccountId  " + str(account['Id']))
-        #print (account['Id'], account['Name'], account['Arn'])
-        #if account['Id'] != rootaccount:
-        #print (account['Status'])
-        #if account['Id'] != '134': #use if you want to leave out ROOT account.
-        if account['Status'] == 'ACTIVE':
+#
+org_client = boto3.client('organizations')
+for account in paginate(org_client.list_accounts):
+    #print (" AccountId  " + str(account['Id']))
+    #print (account['Id'], account['Name'], account['Arn'])
+    #if account['Id'] != rootaccount:
+    #print (account['Status'])
+    #if account['Id'] != '134': #use if you want to leave out ROOT account.
+    if account['Status'] == 'ACTIVE':
+        for region in ec2_client.describe_regions()['Regions']:
+            region_name = region['RegionName']
 
             sc_client=boto3.client('ec2', region_name=region_name)
             try:
