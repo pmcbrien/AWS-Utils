c73b5e01e1c06a2c9c19b9b5610421266bc2ac80
pat mcb <pmcbrien@users.noreply.github.com>
2020-04-10 10:31:59 -0400

    Update snoo.py

--- snoo.py
+++ snoo.py
@@ -12,11 +12,7 @@ for region in ec2_client.describe_regions()['Regions']:
     region_name = region['RegionName']
     ec2 = boto3.client("ec2", region_name=region_name)
     vpc = ec2.describe_vpcs()
+    print ('REGION ' + region_name)
     print(vpc)
     subnets = ec2.describe_subnets()
     print(subnets)
-        #for subnet in vpc.subnets.all():
-        #    print(vpc, "all:", subnet)
-        #for az in ec2.meta.client.describe_availability_zones()["AvailabilityZones"]:
-        #    for subnet in vpc.subnets.filter(Filters=[{"Name": "availabilityZone", "Values": [az["ZoneName"]]}]):
-        #        print(vpc, "filter:", subnet)
