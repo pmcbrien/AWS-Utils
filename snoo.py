bf28ab0cb5e84654660fc25b6f89cc2bb79a20d6
pat mcb <pmcbrien@users.noreply.github.com>
2020-04-10 11:12:12 -0400

    Update snoo.py

--- snoo.py
+++ snoo.py
@@ -2,6 +2,7 @@ import boto3
 import botocore
 import os
 import sys
+import json
 
 # List all regions
 ec2_client = boto3.client('ec2')
@@ -10,9 +11,13 @@ regions = [region['RegionName'] for region in ec2_client.describe_regions()['Reg
 
 for region in ec2_client.describe_regions()['Regions']:
     region_name = region['RegionName']
+    print ('REGION ' + region_name)
+
+    #vpc
     ec2 = boto3.client("ec2", region_name=region_name)
     vpc = ec2.describe_vpcs()
-    print ('REGION ' + region_name)
-    print(vpc)
+    print(json.dumps(vpc))
+
+    #subnets
     subnets = ec2.describe_subnets()
-    print(subnets)
+    print(json.dumps(subnets))
